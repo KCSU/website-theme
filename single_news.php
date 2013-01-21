85ccd918a4fa27d0c56e1997aece980eae01cab2
Gideon Farrell <me@gideonfarrell.co.uk>
2013-01-21 08:51:54 +0000

    Added a news-roll into the news display page

--- single_news.php
+++ single_news.php
@@ -10,11 +10,38 @@
 ?>
 
 <div class="container">
-    <article class="news">
-        <header>
-            <h1><?php the_title(); ?></h1>
-            <p>Posted <span class="date relative"><?php echo get_the_date('Y/m/d H:i:s'); ?></span> by <span class="author"><?php echo the_author_link(); ?></span></p>
-        </header>
+    <div class="row">
+        <article class="news span8">
+            <header>
+                <h1><?php the_title(); ?></h1>
+                <p>Posted <span class="date relative"><?php echo get_the_date('Y/m/d H:i:s'); ?></span> by <span class="author"><?php echo the_author_link(); ?></span></p>
+            </header>
 
-    </article>
+        </article>
+        <section class="roll news span4">
+            <h2>Hot off the press</h2>
+            <ul class="posts-list news">
+            <?php
+                # Need to loop through posts with cat=news
+                
+                query_posts( array ( 'category_name' => 'news', 'posts_per_page' => 10 ) );
+                
+                while (have_posts()){
+                    the_post();
+                    echo string_format(
+                        '<li><!-- {id} --><a href="{link}" title="{title}">{title}</a> <span class="date">{date}</span></li>',
+                        array(
+                            'id'    =>  get_the_ID(),
+                            'link'  =>  get_permalink(),
+                            'title' =>  get_the_title(),
+                            'date'  =>  get_the_date('d/m/Y')
+                        )
+                    );
+                }
+
+                wp_reset_query();
+            ?>
+            </ul>
+        </section>
+    </div>
 </div>
