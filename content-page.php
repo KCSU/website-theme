1638b43e5f80679048b6cecd39a7c5c28b9f021e
Gideon Farrell <me@gideonfarrell.co.uk>
2013-01-31 15:35:05 +0000

    Made edit link a button

--- content-page.php
+++ content-page.php
@@ -13,12 +13,13 @@
 <article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
 	<header class="entry-header">
 		<h1 class="entry-title"><?php the_title(); ?></h1>
+        <?php edit_post_link( 'Edit', '<span class="btn edit-link">', '</span>' ); ?>
 	</header><!-- .entry-header -->
-
+    
 	<div class="entry-content">
 		<?php the_content(); ?>
 	</div><!-- .entry-content -->
 	<footer class="entry-meta">
-		<?php edit_post_link( 'Edit', '<span class="edit-link">', '</span>' ); ?>
+		
 	</footer><!-- .entry-meta -->
 </article><!-- #post-<?php the_ID(); ?> -->
