b71d94849b5bfb40698eedc22c917aecc2daa166
Gideon Farrell <me@gideonfarrell.co.uk>
2013-02-01 08:43:33 +0000

    Added a small copyright notice to the footer

--- footer.php
+++ footer.php
@@ -1,4 +1,8 @@
 <!-- Scripts should really load at the end... -->
 <script src="<?php echo get_template_directory_uri(); ?>/js/require/require.js" data-main="<?php echo get_template_directory_uri(); ?>/js/main.js"></script>
+
+<footer id="Footer">
+    Copyright KCSU 2012
+</footer>
 </body>
 </html>
