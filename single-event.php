1e00b4e1c0eee1b414fd68386bda87bb20acf182
Gideon Farrell <me@gideonfarrell.co.uk>
2013-02-01 08:44:51 +0000

    Events are now a custom post type, so have created a suitable template

--- single-event.php
+++ single-event.php
@@ -7,44 +7,47 @@
  * @author  Gideon Farrell <me@gideonfarrell.co.uk>
  * @license BSD 3-Clause
  */
+
+get_header();
+
+while(have_posts()): the_post();
+$custom_fields = get_post_custom();
 ?>
 
-<div class="container">
+<div class="container events post">
+    <header>
+        <h1><?php the_title(); ?></h1>
+    </header>
+    <hr/>
     <div class="row">
-        <article class="events span8">
-            <header>
-                <h1><?php the_title(); ?></h1>
-            </header>
-
-            <div>
+        <div class="span8">
+            <article>
                 <?php the_content(); ?>
-            </div>
-        </article>
+            </article>
+        </div>
         <div class="span4">
             <section class="events extra">
                 <div>
                     <p class="date"><?php echo get_the_date('l, jS F Y, H:i'); ?></p>
-                    <p class="location"><?php /* we need a location */ ?></p>
+                    <p class="location"><?php echo $custom_fields['location'][0]; ?></p>
                 </div>
             </section>
             <section class="roll events">
                 <h2>Coming up</h2>
                 <ul class="posts-list events">
                 <?php
-                            # Need to loop through posts with cat=events
-                            # TODO: get location of event
-                add_filter('posts_where', 'kcsu_events_filter_where');
-                query_posts( array ( 'category_name'    => 'events',
-                   'posts_per_page'   => 10,
-                   'post_status'      => 'future',
-                   'orderby'          => 'date',
-                   'order'            => 'ascending'
+                # Need to loop through posts with post-type = events
+                # Importantly need to insert date filtering
+                query_posts( array (
+                   'posts_per_page' => 10,
+                   'order'          => 'ascending',
+                   'post_type'      => 'event' 
                    ) );
 
                 while (have_posts()) {
                     the_post();
                     echo string_format(
-                        '<li><!-- {id} --><a href="{link}" title="{title}">{title}</a> <span class="date">{date}</span></li>',
+                        '<li><!-- {id} --><a href="{link}" title="{title}">{title}</a> <span class="aux date">{date}</span></li>',
                         array(
                             'id'    =>  get_the_ID(),
                             'link'  =>  get_permalink(),
@@ -61,3 +64,5 @@
             </section>
         </div>
 </div>
+
+<?php endwhile; get_footer(); ?>
