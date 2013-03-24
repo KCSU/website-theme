e4118266b437b347518cc8d9156c91cb729e25d0
Conor Burgess <Burgess.Conor@gmail.com>
2013-03-24 14:50:28 +0000

    I can haz wp_footer action?

--- footer.php
+++ footer.php
@@ -1,5 +1,7 @@
 <!-- Scripts should really load at the end... -->
 <script src="<?php echo get_template_directory_uri(); ?>/js/require/require.js" data-main="<?php echo get_template_directory_uri(); ?>/js/main.js"></script>
 
+wp_footer();
+
 </body>
 </html>
