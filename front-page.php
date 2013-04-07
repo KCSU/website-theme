55cc9b0828a1ea8b6dc31b8a39b2757630d34b8d
Gideon Farrell <gideon@Gidbook.local>
2013-04-07 22:20:13 +0100

    Added a little "links" column

--- front-page.php
+++ front-page.php
@@ -43,34 +43,43 @@
             ?>
             </ul>
         </div>
-        <!-- Events -->
-        <div id="EventsColumn" class="span4 home-column">
-            <h2>Coming Up</h2>
-            <ul class="posts-list events">
-            <?php
-                # Need to loop through posts with cat=events
-                # TODO: get location of event
-                query_posts( array ( 'posts_per_page'   => 10,
-                                     'orderby'          => 'date',
-                                     'order'            => 'ascending',
-                                     'post_type'        => 'event'
-                                    ) );
-                
-                while (have_posts()) {
-                    the_post();
-                    echo string_format(
-                        '<a href="{link}" title="{title}"><li><!-- {id} -->{title} <span class="aux date">{date}</span></li></a>',
-                        array(
-                            'id'    =>  get_the_ID(),
-                            'link'  =>  get_permalink(),
-                            'title' =>  get_the_title(),
-                            'date'  =>  get_the_date('d/m/Y')
-                        )
-                    );
-                }
-                wp_reset_query();
-            ?>
-            </ul>
+        <div id="RHSColumn" class="span4 home-column">
+            <!-- Events -->
+            <div id="EventsColumn">
+                <h2>Coming Up</h2>
+                <ul class="posts-list events">
+                <?php
+                    # Need to loop through posts with cat=events
+                    # TODO: get location of event
+                    query_posts( array ( 'posts_per_page'   => 10,
+                                         'orderby'          => 'date',
+                                         'order'            => 'ascending',
+                                         'post_type'        => 'event'
+                                        ) );
+                    
+                    while (have_posts()) {
+                        the_post();
+                        echo string_format(
+                            '<a href="{link}" title="{title}"><li><!-- {id} -->{title} <span class="aux date">{date}</span></li></a>',
+                            array(
+                                'id'    =>  get_the_ID(),
+                                'link'  =>  get_permalink(),
+                                'title' =>  get_the_title(),
+                                'date'  =>  get_the_date('d/m/Y')
+                            )
+                        );
+                    }
+                    wp_reset_query();
+                ?>
+                </ul>
+            </div>
+            <!-- Quick Links -->
+            <div id="LinksColumn">
+                <h2>Getting Around</h2>
+                <ul class="links-list">
+                    <?php /* need some sort of menu here (yes, I'm resurrecting it) */ ?>
+                </ul>
+            </div>
         </div>
     </div>
 </div>
