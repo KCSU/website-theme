25c4b0c2db6402d1844fa48626bb5aec0ec343d3
Conor Burgess <Burgess.Conor@gmail.com>
2013-11-18 19:26:43 +0000

    Fix 404 template

--- 404.php
+++ 404.php
@@ -11,21 +11,17 @@
 
 get_header(); ?>
 
-	<div id="primary">
-		<div id="content" role="main">
-
-			<article id="post-0" class="post error404 not-found">
-				<header class="entry-header">
-					<h1 class="entry-title">This is somewhat embarrassing, isn&rsquo;t it?</h1>
-				</header>
-
-				<div class="entry-content">
-					<p>It seems we can&rsquo;t find what you&rsquo;re looking for.</p>
-
-				</div><!-- .entry-content -->
-			</article><!-- #post-0 -->
-
-		</div><!-- #content -->
-	</div><!-- #primary -->
+<div class="container post">
+    <header>
+        <h1>This is somewhat embarrassing, isn&rsquo;t it?</h1>
+    </header>
+    <div class="row">
+        <div class="span8">            
+            <article>
+                <p>It seems we can&rsquo;t find what you&rsquo;re looking for.</p>
+            </article>
+        </div>
+    </div>
+</div>
 
 <?php get_footer(); ?>
