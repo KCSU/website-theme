a22649302aecc56ffa2493f8442f2c76b393e28b
Conor Burgess <Burgess.Conor@gmail.com>
2015-08-01 01:36:46 +0100

    Add support for marketplace
    Backend of marketplace. Requires appropriate capabilities to be added to users (edit_classifieds, publish_classifieds etc.).

--- custom_fields.php
+++ custom_fields.php
@@ -25,20 +25,6 @@ if( !$acf )
     include_once(dirname(__file__) . '/lib/advanced-custom-fields/acf.php');
 }
 
-// Fields 
-add_action('acf/register_fields', 'my_register_fields');
-
-function my_register_fields()
-{
-    //include_once('add-ons/acf-repeater/repeater.php');
-    //include_once('add-ons/acf-gallery/gallery.php');
-    //include_once('add-ons/acf-flexible-content/flexible-content.php');
-}
-
-// Options Page 
-// include_once( 'add-ons/acf-options-page/acf-options-page.php' );
-
-
 /**
  *  Register Field Groups
  *
@@ -90,6 +76,10 @@ if(function_exists("register_field_group"))
                 0 => 'author',
                 1 => 'categories',
                 2 => 'tags',
+                3 => 'custom_fields',
+				4 => 'discussion',
+				5 => 'comments',
+				6 => 'revisions',
             ),
         ),
         'menu_order' => 0,
@@ -144,6 +134,13 @@ if(function_exists("register_field_group"))
             'position' => 'normal',
             'layout' => 'no_box',
             'hide_on_screen' => array (
+                0 => 'author',
+                1 => 'categories',
+                2 => 'tags',
+                3 => 'custom_fields',
+				4 => 'discussion',
+				5 => 'comments',
+				6 => 'revisions',
             ),
         ),
         'menu_order' => 0,
@@ -198,9 +195,172 @@ if(function_exists("register_field_group"))
             'position' => 'normal',
             'layout' => 'no_box',
             'hide_on_screen' => array (
+                0 => 'author',
+                1 => 'categories',
+                2 => 'tags',
+                3 => 'custom_fields',
+				4 => 'discussion',
+				5 => 'comments',
+				6 => 'revisions',
             ),
         ),
         'menu_order' => 0,
     ));
+	register_field_group(array (
+		'id' => 'acf_marketplace',
+		'title' => 'Marketplace',
+		'fields' => array (
+			array (
+				'key' => 'field_55bb952cdd413',
+				'label' => 'Information',
+				'name' => '',
+				'type' => 'message',
+				'instructions' => 'This will be automatically set to two weeks after publishing your ad.',
+				'message' => 'Your ad will expire two weeks after the publish date. If you want to extend this, come back and edit the published date accordingly.
+	
+	You can use the content editor at the bottom to add a description and pictures.',
+			),
+			array (
+				'key' => 'field_55bb8de5bd12f',
+				'label' => 'Ad Type',
+				'name' => 'ad_type',
+				'type' => 'radio',
+				'required' => 1,
+				'choices' => array (
+					'Offering' => 'Offering',
+					'Wanted' => 'Wanted',
+				),
+				'other_choice' => 0,
+				'save_other_choice' => 0,
+				'default_value' => 'Offering',
+				'layout' => 'horizontal',
+			),
+			array (
+				'key' => 'field_55bb8e99bd130',
+				'label' => 'Category',
+				'name' => 'category',
+				'type' => 'select',
+				'required' => 1,
+				'choices' => array (
+					'Books' => 'Books',
+					'Clothes' => 'Clothes',
+					'Formal Tickets' => 'Formal Tickets',
+					'Bikes' => 'Bikes',
+					'Room Decor' => 'Room Decor',
+					'Kitchen' => 'Kitchen',
+					'Other' => 'Other',
+				),
+				'default_value' => '',
+				'allow_null' => 0,
+				'multiple' => 0,
+			),
+			array (
+				'key' => 'field_55bb9467dd410',
+				'label' => 'Swap or Sell',
+				'name' => 'swap_or_sell',
+				'type' => 'radio',
+				'required' => 1,
+				'choices' => array (
+					'Swap' => 'Swap',
+					'Sell' => 'Sell',
+				),
+				'other_choice' => 0,
+				'save_other_choice' => 0,
+				'default_value' => 'Sell',
+				'layout' => 'horizontal',
+			),
+			array (
+				'key' => 'field_55bb948cdd411',
+				'label' => 'Asking price',
+				'name' => 'asking_price',
+				'type' => 'text',
+				'required' => 1,
+				'conditional_logic' => array (
+					'status' => 1,
+					'rules' => array (
+						array (
+							'field' => 'field_55bb9467dd410',
+							'operator' => '==',
+							'value' => 'Sell',
+						),
+					),
+					'allorany' => 'all',
+				),
+				'default_value' => '',
+				'placeholder' => '',
+				'prepend' => '',
+				'append' => '',
+				'formatting' => 'html',
+				'maxlength' => '',
+			),
+			array (
+				'key' => 'field_55bb94dadd412',
+				'label' => 'Swap for',
+				'name' => 'swap_for',
+				'type' => 'text',
+				'instructions' => 'Suggestions of what you\'d like to swap for',
+				'conditional_logic' => array (
+					'status' => 1,
+					'rules' => array (
+						array (
+							'field' => 'field_55bb9467dd410',
+							'operator' => '==',
+							'value' => 'Swap',
+						),
+					),
+					'allorany' => 'all',
+				),
+				'default_value' => '',
+				'placeholder' => '',
+				'prepend' => '',
+				'append' => '',
+				'formatting' => 'html',
+				'maxlength' => '',
+			),
+			array (
+				'key' => 'field_55bb973cdd415',
+				'label' => 'Sold',
+				'name' => 'sold',
+				'type' => 'checkbox',
+				'instructions' => 'Tick once a swap or sale has been agreed to hide your ad.',
+				'choices' => array (
+					'My item has been swapped or sold' => 'My item has been swapped or sold',
+				),
+				'default_value' => '',
+				'layout' => 'vertical',
+			),
+		),
+		'location' => array (
+			array (
+				array (
+					'param' => 'post_type',
+					'operator' => '==',
+					'value' => 'classified',
+					'order_no' => 0,
+					'group_no' => 0,
+				),
+			),
+		),
+		'options' => array (
+			'position' => 'acf_after_title',
+			'layout' => 'default',
+			'hide_on_screen' => array (
+				0 => 'permalink',
+				1 => 'excerpt',
+				2 => 'custom_fields',
+				3 => 'discussion',
+				4 => 'comments',
+				5 => 'revisions',
+				6 => 'slug',
+				7 => 'author',
+				8 => 'format',
+				9 => 'featured_image',
+				10 => 'categories',
+				11 => 'tags',
+				12 => 'send-trackbacks',
+			),
+		),
+		'menu_order' => 0,
+	));
 }
 ?>
--- custom_post_types.php
+++ custom_post_types.php
@@ -1,87 +1,91 @@
 <?php
-/**
- * Register custom post types
- */
-add_action( 'init', 'create_post_types' );
-add_action( 'admin_menu', 'kcsu_remove_menu_items' );
     
-function create_post_types() {
-    register_post_type('exec',
-        array(
-            'labels' => array(
-                'name'          => __('The Exec'),
-                'singular_name' => __('Exec'),
-                'all_items'     => __('The Whole Exec'),
-                'add_new_item'  => __('New Member'),
-                'edit_item'     => __('Edit Profile'),
-                'new_item'      => __('New Profile'),
-                'view_item'     => __('View Profile')
-            ),
-            'public'        => true,
-            'heirarchical'  => false,
-            'rewrite'       => array('slug' => 'exec'),
-            'supports'      => array('title', 'thumbnail', 'editor', 'excerpt')
-        )
-    );
-    register_post_type('event',
-        array(
-            'labels' => array(
-                'name'          => __('Events'),
-                'singular_name' => __('Event'),
-                'all_items'     => __('All Events'),
-                'add_new_item'  => __('New Event'),
-                'edit_item'     => __('Edit Event'),
-                'new_item'      => __('New Event'),
-                'view_item'     => __('View Event')
-            ),
-            'public'        => true,
-            'heirarchical'  => false,
-            'rewrite'       => array('slug' => 'events'),
-            'supports'      => array('title', 'author', 'editor')
-        )
-    );
-    register_post_type('external-event',
-        array(
-            'labels' => array(
-                'name'          => __('External Events'),
-                'singular_name' => __('External Event'),
-                'all_items'     => __('All External Events'),
-                'add_new_item'  => __('New External Event'),
-                'edit_item'     => __('Edit External Event'),
-                'new_item'      => __('New External Event'),
-                'view_item'     => __('View External Event')
-            ),
-            'public'        => true,
-            'heirarchical'  => false,
-            'rewrite'       => array('slug' => 'external-events'),
-            'supports'      => array('title', 'author', 'editor')
-        )
-    );
-    register_post_type('classifieds',
-        array(
-            'labels' => array(
-                'name'          => __('Marketplace Ads'),
-                'singular_name' => __('Marketplace Ad'),
-                'all_items'     => __('All Marketplace Ads'),
-                'add_new_item'  => __('New Marketplace Ad'),
-                'edit_item'     => __('Edit Marketplace Ad'),
-                'new_item'      => __('New Marketplace Ad'),
-                'view_item'     => __('View Marketplace Ad')
-            ),
-            'public'          => true,
-            'heirarchical'    => false,
-            'capability_type' => 'marketplace'
-            'rewrite'         => array('slug' => 'marketplace'),
-            'supports'        => array('title', 'author', 'editor')
-        )
-    );
-}
-
-function kcsu_remove_menu_items() {
-    if (!current_user_can( 'edit_post' )) {
-        remove_menu_page( 'edit.php?post_type=exec' );
-        remove_menu_page( 'edit.php?post_type=event' );
-        remove_menu_page( 'edit.php?post_type=external-event' );
+    /**
+     * Register custom post types
+     */
+    
+    function kcsu_create_post_types() {
+        register_post_type('exec',
+            array(
+                'labels' => array(
+                    'name'          => __('The Exec'),
+                    'singular_name' => __('Exec'),
+                    'all_items'     => __('The Whole Exec'),
+                    'add_new_item'  => __('New Member'),
+                    'edit_item'     => __('Edit Profile'),
+                    'new_item'      => __('New Profile'),
+                    'view_item'     => __('View Profile')
+                ),
+                'public'        => true,
+                'heirarchical'  => false,
+                'rewrite'       => array('slug' => 'exec'),
+                'supports'      => array('title', 'thumbnail', 'editor', 'excerpt')
+            )
+        );
+        register_post_type('event',
+            array(
+                'labels' => array(
+                    'name'          => __('Events'),
+                    'singular_name' => __('Event'),
+                    'all_items'     => __('All Events'),
+                    'add_new_item'  => __('New Event'),
+                    'edit_item'     => __('Edit Event'),
+                    'new_item'      => __('New Event'),
+                    'view_item'     => __('View Event')
+                ),
+                'public'        => true,
+                'heirarchical'  => false,
+                'rewrite'       => array('slug' => 'events'),
+                'supports'      => array('title', 'author', 'editor')
+            )
+        );
+        register_post_type('external-event',
+            array(
+                'labels' => array(
+                    'name'          => __('External Events'),
+                    'singular_name' => __('External Event'),
+                    'all_items'     => __('All External Events'),
+                    'add_new_item'  => __('New External Event'),
+                    'edit_item'     => __('Edit External Event'),
+                    'new_item'      => __('New External Event'),
+                    'view_item'     => __('View External Event')
+                ),
+                'public'        => true,
+                'heirarchical'  => false,
+                'rewrite'       => array('slug' => 'external-events'),
+                'supports'      => array('title', 'author', 'editor')
+            )
+        );
+        register_post_type('classified',
+            array(
+                'labels' => array(
+                    'name'          => __('Marketplace Ads'),
+                    'singular_name' => __('Marketplace Ad'),
+                    'all_items'     => __('All Marketplace Ads'),
+                    'add_new_item'  => __('New Marketplace Ad'),
+                    'edit_item'     => __('Edit Marketplace Ad'),
+                    'new_item'      => __('New Marketplace Ad'),
+                    'view_item'     => __('View Marketplace Ad')
+                ),
+                'public'          => true,
+                'heirarchical'    => false,
+                'capability_type' => 'classified',
+                'map_meta_cap'    => true,
+                'rewrite'         => array('slug' => 'marketplace'),
+                'supports'        => array('title', 'author', 'editor')
+            )
+        );
     }
-}
+
+    function kcsu_remove_menu_items() {
+        if (!current_user_can( 'edit_posts' )) {
+            remove_menu_page( 'edit.php?post_type=exec' );
+            remove_menu_page( 'edit.php?post_type=event' );
+            remove_menu_page( 'edit.php?post_type=external-event' );
+        }
+    }
+    
+    add_action( 'init', 'kcsu_create_post_types' );
+    add_action( 'admin_menu', 'kcsu_remove_menu_items' );
+    
 ?>
